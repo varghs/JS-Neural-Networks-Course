8ae157e8c4f67e068376bbee6fc6d24ea0f7687a
xXH4CKST3RXx <52459831+xXH4CKST3RXx@users.noreply.github.com>
2019-09-20 23:02:10 -0400

    Update howNeuralNetworksLearn_counter.js

--- howNeuralNetworksLearn_counter.js
+++ howNeuralNetworksLearn_counter.js
@@ -4,6 +4,8 @@
 const trainingData = [
   [1, 2, 3, 4, 5],
   [5, 4, 3, 2, 1],
+  [6, 7, 8, 9, 10],
+  [10, 9, 8, 7, 6]
 ];
 
 const net = new brain.recurrent.LSTMTimeStep();
@@ -12,3 +14,9 @@ net.train(trainingData);
 
 console.log(net.run([1, 2, 3, 4]));
 console.log(net.run([5, 4, 3, 2]));
+
+// bonus
+
+// 6-10, 10-6
+console.log(net.run([6, 7, 8, 9]));
+console.log(net.run([10, 9, 8, 7]));
